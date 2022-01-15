bbd1176cb0714adfa7eb2fdb4ddecf9032f54485
GlaucileneSilva <glauhzinhah@gmail.com>
2022-01-15 19:58:58 -0300

    CadastroDeEventos.js

---
+++ CadastroDeEventos.js
@@ -0,0 +1,31 @@
+var data = new Date();
+
+var DataDoEvento = 02/01/2022
+if (DataDoEvento => new Date){
+console.log("Evento permitido")
+}else{
+    console.log("Evento não permitido, escolha outra data.")
+}
+
+
+var ListaDeParticipantes = ["Glaucilene","Tamires","Maria","Marcio","Henrique"]
+
+if (ListaDeParticipantes.length < 100){
+    // Seu nome foi incluído na lista de participantes
+    console.log("Seu nome foi incluído na lista de participantes")
+} else {
+    console.log("Infelizmente não temos mais vagas")
+}
+
+
+var IdadeDoParticipante = 18;
+if (IdadeDoParticipante.length > 18){
+    // Idade Permitida para Cadastro
+    console.log("Idade permitida para cadastro")
+} else {
+    console.log("Não é permitido o cadastro para menores de 18 anos")
+}
+
+
+
+
